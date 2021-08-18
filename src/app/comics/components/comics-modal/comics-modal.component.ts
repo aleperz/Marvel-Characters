4f31d36fa15e9329f108d937f06bddedeba64fba
aleperz <alejandraperez.659@gmail.com>
2021-08-17 22:19:12 -0500

    style settings

--- src/app/comics/components/comics-modal/comics-modal.component.ts
+++ src/app/comics/components/comics-modal/comics-modal.component.ts
@@ -1,38 +1,36 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
-import { ComicId } from '../../models/comics.interface';
-import { FavoriteComicsService } from '../../services/favorite-comics.service';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
+import { ComicId } from '../../models/comics.interface'
+import { FavoriteComicsService } from '../../services/favorite-comics.service'
 
 @Component({
   selector: 'app-comics-modal',
   templateUrl: './comics-modal.component.html',
-  styleUrls: ['./comics-modal.component.css'],
+  styleUrls: ['./comics-modal.component.css']
 })
 export class ComicsModalComponent implements OnInit {
-  @Input() comic!: ComicId;
-  @Output() closeModal = new EventEmitter<void>();
+  @Input() comic!: ComicId
+  @Output() closeModal = new EventEmitter<void>()
 
-  ComicOnList!: boolean;
-  comicFavList!: ComicId[];
+  ComicOnList!: boolean
+  comicFavList!: ComicId[]
 
-  comicInFavoriteList: boolean = true;
+  comicInFavoriteList = true
 
   constructor(private favoritesService: FavoriteComicsService) {}
 
   ngOnInit(): void {
-    console.log(this.comic);
+    console.log(this.comic)
   }
-  close() {
-    this.closeModal.emit();
+  close(): void {
+    this.closeModal.emit()
   }
   addFavoriteComic() {
-    this.favoritesService.addToFavoritesList(this.comic);
+    this.favoritesService.addToFavoritesList(this.comic)
   }
 
   validateFavoriteList() {
-    this.comicFavList = this.favoritesService.getFavoritesList();
+    this.comicFavList = this.favoritesService.getFavoritesList()
 
-    return this.comicFavList.some(
-      (favoriteComic) => favoriteComic.title === this.comic.title
-    );
+    return this.comicFavList.some((favoriteComic) => favoriteComic.title === this.comic.title)
   }
 }
--- src/app/shared/components/modal-button/modal-button.component.ts
+++ src/app/shared/components/modal-button/modal-button.component.ts
@@ -1,24 +1,25 @@
-import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core'
 
 @Component({
   selector: 'app-modal-button',
   templateUrl: './modal-button.component.html',
-  styleUrls: ['./modal-button.component.css'],
+  styleUrls: ['./modal-button.component.css']
 })
 export class ModalButtonComponent implements OnInit {
-  @Input() urlImg: string = '';
-  @Input() text: string = '';
-  @Input() type: string = '';
-  @Output() addToFavorite = new EventEmitter<void>();
+  @Input() urlImg = ''
+  @Input() text = ''
+  @Input() type = ''
+  @Input() urlPurchase = ''
+  @Output() addToFavorite = new EventEmitter<void>()
 
   constructor() {}
 
   ngOnInit(): void {}
 
   addFavorite() {
-    this.addToFavorite.emit();
+    this.addToFavorite.emit()
   }
   buyComic() {
-    console.log('comprar comic');
+    console.log('comprar comic')
   }
 }
